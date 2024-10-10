bf4c38af9ccc017b98405e6284de546fffbf0ecc
Xi Xu <i@xi-xu.me>
2024-10-11 04:25:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ const generalConfig = {
             "https://cdn.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geosite.dat",
         mmdb: "https://cdn.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/country-lite.mmdb",
     },
-    "global-ua": "\u0063\u006c\u0061\u0073\u0068.meta",
+    "global-ua": "\u0063\u006c\u0061\u0073\u0068.\u006D\u0065\u0074\u0061",
     "etag-support": true,
 };
 
